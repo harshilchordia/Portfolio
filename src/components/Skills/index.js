bcd0b26458eabc813bd1e47acfa7c54ab9e79c02
Harshil <harshil.chordia@gmail.com>
2022-10-03 02:33:51 +0100

    skill section better

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,6 +1,9 @@
 import "./index.scss";
-import React, { useState, useEffect } from "react";
 import AnimatedLetters from "../AnimatedLetters";
+
+import { Box, Tab } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import React, { useState, useEffect } from "react";
 
 const Skills = () => {
 	const [letterClass, setLetterClass] = useState("text-animate");
@@ -11,48 +14,66 @@ const Skills = () => {
 			return setLetterClass("text-animate-hover");
 		}, 4000);
 	}, []);
+	const [value, setValue] = useState("1");
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
+	const langauges = ["Python", "Java", "JavaScript", "PHP", "HTML", "CSS", "Sass"];
+
+	const sx_Tabs = {
+		color: " #521707",
+		textTransform: "none",
+		fontWeight: 900,
+		fontSize: "16px",
+		fontFamily: [
+			'"Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", "Arial", "sans-serif"',
+		].join(","),
+		"&:hover": {
+			color: "#450808",
+			opacity: 0.9,
+		},
+	};
 	return (
 		<div className="skillContainer" id="skills">
 			<h1>
 				<AnimatedLetters letterClass={letterClass} strArray={nameArray} index={15} />
 			</h1>
 			<div className="skillContent">
-				<div className="column1">
-					<h3>Languages</h3>
-					Python
-					<br />
-					Java
-					<br />
-					JavaScript
-					<br />
-				</div>
-				<div className="column2">
-					<h3>Frameworks</h3>
-					React
-					<br />
-				</div>
-				<div className="column3">
-					<h3>Tools</h3>
-				</div>
-
-				{/* <div className="table">
-				<table>
-					<tbody>
-						<tr>
-							<th>Languages</th>
-							<th>Frameworks</th>
-							<th>Tools</th>
-						</tr>
-						<tr>
-							<td>Python</td>
-							<td>React</td>
-						</tr>
-						<tr>
-							<td>Java</td>
-						</tr>
-					</tbody>
-				</table>
-			</div> */}
+				<Box>
+					<TabContext value={value}>
+						<Box sx={{ borderLeft: 3, borderColor: "divider" }} className="titles">
+							<TabList
+								onChange={handleChange}
+								textColor="inherit"
+								indicatorColor="secondary"
+								centered
+								orientation="vertical"
+								TabIndicatorProps={{ style: { background: "#b85303" } }}
+							>
+								<Tab label="Languages" value="1" sx={sx_Tabs} />
+								<Tab label="Framework" value="2" sx={sx_Tabs} />
+								<Tab label="Tools" value="3" sx={sx_Tabs} />
+							</TabList>
+						</Box>
+						<div className="textPanel">
+							<TabPanel value="1">
+								{langauges.map((item) => {
+									return (
+										<>
+											<p>
+												{item} <br />
+											</p>
+										</>
+									);
+								})}
+							</TabPanel>
+							<TabPanel value="2">React</TabPanel>
+							<TabPanel value="3">Panel three</TabPanel>
+						</div>
+					</TabContext>
+				</Box>
 			</div>
 		</div>
 	);
